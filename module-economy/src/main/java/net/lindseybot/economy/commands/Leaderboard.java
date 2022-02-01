3b0e7ce4eca13b68ad10247d119bfcfc547f00d9
Fabricio Winter <git@notfab.net>
2022-01-31 21:21:12 -0300

    Update leaderboard

--- module-economy/src/main/java/net/lindseybot/economy/commands/Leaderboard.java
+++ module-economy/src/main/java/net/lindseybot/economy/commands/Leaderboard.java
@@ -1,31 +1,53 @@
 package net.lindseybot.economy.commands;
 
 import lombok.extern.slf4j.Slf4j;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.sharding.ShardManager;
+import net.lindseybot.economy.properties.ImageGenProperties;
 import net.lindseybot.economy.repositories.sql.UserProfileRepository;
-import net.lindseybot.shared.entities.discord.FMessage;
+import net.lindseybot.economy.services.CustomizationService;
 import net.lindseybot.shared.entities.discord.Label;
-import net.lindseybot.shared.entities.discord.builders.EmbedBuilder;
 import net.lindseybot.shared.entities.profile.UserProfile;
+import net.lindseybot.shared.entities.profile.users.Customization;
+import net.lindseybot.shared.enums.Flags;
 import net.lindseybot.shared.enums.LeaderboardType;
 import net.lindseybot.shared.worker.InteractionHandler;
 import net.lindseybot.shared.worker.SlashCommand;
 import net.lindseybot.shared.worker.services.Messenger;
+import okhttp3.*;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.util.Locale;
+
 @Slf4j
 @Component
 public class Leaderboard extends InteractionHandler {
 
-    private final UserProfileRepository repository;
 
-    public Leaderboard(Messenger msg, UserProfileRepository repository) {
+    private final ShardManager jda;
+    private final String endpoint;
+    private final OkHttpClient client;
+    private final UserProfileRepository repository;
+    private final CustomizationService customizations;
+
+    public Leaderboard(Messenger msg, ShardManager jda,
+                       UserProfileRepository repository,
+                       ImageGenProperties properties,
+                       CustomizationService customizations) {
         super(msg);
+        this.jda = jda;
         this.repository = repository;
+        this.client = new OkHttpClient();
+        this.customizations = customizations;
+        this.endpoint = "http://" + properties.getIp() + ":" + properties.getPort() + "/leaderboards";
     }
 
     @SlashCommand("leaderboard")
@@ -33,20 +55,72 @@ public class Leaderboard extends InteractionHandler {
         LeaderboardType type = LeaderboardType.fromString(
                 this.getOption("name", event, String.class)
         );
-        StringBuilder msg = new StringBuilder("```\n");
-        int pos = 1;
+        int pos = 0;
+        JSONArray data = new JSONArray();
         for (UserProfile profile : this.getPage(type, 0)) {
-            msg.append("#").append(pos)
-                    .append(": ").append(profile.getName())
-                    .append(" - ").append(this.getValue(type, profile))
-                    .append("\n");
             pos++;
+            User user = this.getUser(profile.getUser());
+
+            String name = profile.getName();
+            if (name == null && user != null) {
+                name = profile.getName();
+            }
+
+            String avatar = null;
+            if (user != null) {
+                avatar = user.getEffectiveAvatarUrl();
+            }
+
+            JSONObject object = new JSONObject();
+            object.put("position", pos);
+            object.put("username", name == null ? "Anonymous" : name);
+            object.put("image", avatar);
+            Customization customization = this.customizations.getCustomization(profile.getUser());
+            if (customization.getBackground() != null) {
+                JSONObject bg = new JSONObject();
+                bg.put("id", customization.getBackground().getId());
+                bg.put("fontColor", customization.getBackground().getFontColor());
+                object.put("background", bg);
+            }
+            if (profile.getCountry() != null
+                    && profile.getCountry() != Flags.Unknown) {
+                object.put("country", profile.getCountry().name().toLowerCase(Locale.ROOT));
+            }
+            object.put("value", this.getValue(type, profile));
+            data.put(object);
         }
-        msg.append("```");
-        EmbedBuilder embed = new EmbedBuilder();
-        embed.description(Label.raw(msg.toString()));
-        embed.title(Label.raw(type.getPrettyName()));
-        this.msg.reply(event, FMessage.of(embed.build()));
+        RequestBody reqBody = RequestBody.create(MediaType.parse("application/json"), data.toString());
+        Request request = new Request.Builder()
+                .url(this.endpoint)
+                .post(reqBody)
+                .build();
+        Call call = client.newCall(request);
+        try (Response response = call.execute(); ResponseBody body = response.body()) {
+            if (!response.isSuccessful() || body == null) {
+                this.msg.error(event, Label.of("error.internal"));
+                return;
+            }
+            byte[] bytes = body.bytes();
+            if (event.isAcknowledged()) {
+                event.getHook().editOriginal(bytes, "profile.png")
+                        .queue();
+            } else {
+                event.getHook().sendFile(bytes, "profile.png")
+                        .queue();
+            }
+        } catch (IOException ex) {
+            log.error("Failed to request profile", ex);
+        }
+    }
+
+    private User getUser(long userId) {
+        User user = this.jda.getUserById(userId);
+        if (user != null) {
+            return user;
+        } else {
+            return this.jda.retrieveUserById(userId)
+                    .complete();
+        }
     }
 
     private String getValue(LeaderboardType type, UserProfile profile) {
@@ -63,7 +137,7 @@ public class Leaderboard extends InteractionHandler {
             case SLOT_WINS -> Sort.by(Sort.Direction.DESC, "slotWins");
             case DAILY_STREAK -> Sort.by(Sort.Direction.DESC, "cookieStreak");
         };
-        Pageable pageable = PageRequest.of(number, 15, sort);
+        Pageable pageable = PageRequest.of(number, 10, sort);
         return this.repository.findAll(pageable);
     }
 
