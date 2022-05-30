3b1913ce33d1594c982cf18c8ec5bd954c4bf283
Fabricio Winter <git@notfab.net>
2022-05-30 04:30:33 -0300

    Remove local repository

--- module-points/src/main/java/net/lindseybot/points/repositories/PointConfigRepository.java
+++
@@ -1,7 +0,0 @@
-package net.lindseybot.points.repositories;
-
-import net.lindseybot.points.entities.PointConfig;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PointConfigRepository extends JpaRepository<PointConfig, Long> {
-}
